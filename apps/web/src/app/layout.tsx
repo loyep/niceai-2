79c65def9df58e46517079b9d4ffc8e833791e3a
Zack <lorenx@163.com>
2024-01-13 16:35:04 +0800

    add theme provider

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,18 +1,17 @@
 import type { Metadata, Viewport } from "next";
-// import { cache } from "react";
-// import { headers } from "next/headers";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 
+import { ArcoThemeProvider } from "./theme-provider";
+
 import "@arco-themes/react-niceai/css/arco.css";
 
 import { cn } from "@niceai/ui";
-import { ThemeProvider, ThemeToggle } from "@niceai/ui/theme";
+import { ThemeProvider } from "./theme-provider";
 
 import { env } from "~/env";
-// import { TRPCReactProvider } from "~/trpc/react";
 
 import "~/app/globals.css";
 
@@ -36,8 +35,6 @@ export const viewport: Viewport = {
   ],
 };
 
-// const getHeaders = cache(async () => headers());
-
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const session = await auth();
   return (
@@ -54,14 +51,14 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
             attribute="class"
             defaultTheme="system"
             enableSystem
-            enableColorScheme={false}
+            disableTransitionOnChange
           >
-            {/* <TRPCReactProvider headersPromise={getHeaders()}> */}
-            {props.children}
-            {/* </TRPCReactProvider> */}
-            <div className="absolute bottom-4 right-4">
-              <ThemeToggle />
-            </div>
+            <ArcoThemeProvider>
+              {props.children}
+              {/* <div className="absolute bottom-4 right-4">
+                <ThemeToggle />
+              </div> */}
+            </ArcoThemeProvider>
           </ThemeProvider>
         </SessionProvider>
         <SpeedInsights />
---
+++ apps/web/src/app/theme-provider.tsx
@@ -0,0 +1,488 @@
+"use client";
+
+import React, {
+  createContext,
+  Fragment,
+  memo,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from "react";
+import { ConfigProvider } from "@arco-design/web-react";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+
+import { Button } from "@niceai/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@niceai/ui/dropdown-menu";
+
+type ValueObject = Record<string, string>;
+
+interface UseThemeProps {
+  /** List of all available theme names */
+  themes: string[];
+  /** Forced theme name for the current page */
+  forcedTheme?: string | undefined;
+  /** Update the theme */
+  setTheme: React.Dispatch<React.SetStateAction<string>>;
+  /** Active theme name */
+  theme?: string | undefined;
+  /** If `enableSystem` is true and the active theme is "system", this returns whether the system preference resolved to "dark" or "light". Otherwise, identical to `theme` */
+  resolvedTheme?: string | undefined;
+  /** If enableSystem is true, returns the System theme preference ("dark" or "light"), regardless what the active theme is */
+  systemTheme?: "dark" | "light" | undefined;
+}
+
+interface ThemeProviderProps {
+  /** List of all available theme names */
+  themes?: string[] | undefined;
+  /** Forced theme name for the current page */
+  forcedTheme?: string | undefined;
+  /** Whether to switch between dark and light themes based on prefers-color-scheme */
+  enableSystem?: boolean | undefined;
+  /** Disable all CSS transitions when switching themes */
+  disableTransitionOnChange?: boolean | undefined;
+  /** Whether to indicate to browsers which color scheme is used (dark or light) for built-in UI like inputs and buttons */
+  enableColorScheme?: boolean | undefined;
+  /** Key used to store theme setting in localStorage */
+  storageKey?: string | undefined;
+  /** Default theme name (for v0.0.12 and lower the default was light). If `enableSystem` is false, the default theme is light */
+  defaultTheme?: string | undefined;
+  /** HTML attribute modified based on the active theme. Accepts `class` and `data-*` (meaning any data attribute, `data-mode`, `data-color`, etc.) */
+  attribute?: "class" | undefined;
+  /** Mapping of theme name to HTML attribute value. Object where key is the theme name and value is the attribute value */
+  value?: ValueObject | undefined;
+  /** Nonce string to pass to the inline script for CSP headers */
+  nonce?: string | undefined;
+
+  children?: React.ReactNode;
+}
+
+const colorSchemes = ["light", "dark"];
+const MEDIA = "(prefers-color-scheme: dark)";
+const isServer = typeof window === "undefined";
+const ThemeContext = createContext<UseThemeProps | undefined>(undefined);
+const defaultContext: UseThemeProps = {
+  setTheme: (_) => {
+    //
+  },
+  themes: [],
+};
+
+export const useTheme = () => useContext(ThemeContext) ?? defaultContext;
+
+export const ThemeProvider: React.FC<ThemeProviderProps> = (props) => {
+  const context = useContext(ThemeContext);
+
+  // Ignore nested context providers, just passthrough children
+  if (context) return <Fragment>{props.children}</Fragment>;
+  return <Theme {...props} />;
+};
+
+const defaultThemes = ["light", "dark"];
+
+const Theme: React.FC<ThemeProviderProps> = ({
+  forcedTheme,
+  disableTransitionOnChange = false,
+  enableSystem = true,
+  enableColorScheme = true,
+  storageKey = "theme",
+  themes = defaultThemes,
+  defaultTheme = enableSystem ? "system" : "light",
+  attribute = "data-theme",
+  value,
+  children,
+  nonce,
+}) => {
+  const [theme, setThemeState] = useState(() =>
+    getTheme(storageKey, defaultTheme),
+  );
+  const [resolvedTheme, setResolvedTheme] = useState(() =>
+    getTheme(storageKey),
+  );
+  const attrs = !value ? themes : Object.values(value);
+
+  const applyTheme = useCallback(
+    (theme: string) => {
+      let resolved = theme;
+      if (!resolved) return;
+
+      // If theme is system, resolve it before setting theme
+      if (theme === "system" && enableSystem) {
+        resolved = getSystemTheme();
+      }
+
+      const name = value ? value[resolved] : resolved;
+      const enable = disableTransitionOnChange ? disableAnimation() : null;
+      const d = document.documentElement;
+      const a = document.body
+
+      if (attribute === "class") {
+        d.classList.remove(...attrs);
+
+        if (name) d.classList.add(name);
+      } else {
+        if (name) {
+          d.setAttribute(attribute, name);
+        } else {
+          d.removeAttribute(attribute);
+        }
+      }
+      a.setAttribute('arco-theme', name!)
+
+      if (enableColorScheme) {
+        const fallback = colorSchemes.includes(defaultTheme)
+          ? defaultTheme
+          : null;
+        const colorScheme = colorSchemes.includes(resolved)
+          ? resolved
+          : fallback;
+        d.style.colorScheme = colorScheme!;
+      }
+
+      enable?.();
+    },
+    [
+      attribute,
+      attrs,
+      defaultTheme,
+      disableTransitionOnChange,
+      enableColorScheme,
+      enableSystem,
+      value,
+    ],
+  );
+
+  const setTheme = useCallback(
+    (themeFn: string | ((t?: string) => string)) => {
+      const newTheme = typeof themeFn === "function" ? themeFn(theme) : themeFn;
+      setThemeState(newTheme);
+
+      // Save to storage
+      try {
+        console.log('1111111', newTheme)
+        localStorage.setItem(storageKey, newTheme);
+      } catch (e) {
+        // Unsupported
+      }
+    },
+    [storageKey, theme],
+  );
+
+  const handleMediaQuery = useCallback(
+    (e: MediaQueryListEvent | MediaQueryList) => {
+      const resolved = getSystemTheme(e);
+      setResolvedTheme(resolved);
+
+      if (theme === "system" && enableSystem && !forcedTheme) {
+        applyTheme("system");
+      }
+    },
+    [theme, enableSystem, forcedTheme, applyTheme],
+  );
+
+  // Always listen to System preference
+  useEffect(() => {
+    const media = window.matchMedia(MEDIA);
+
+    // Intentionally use deprecated listener methods to support iOS & old browsers
+    media.addListener(handleMediaQuery);
+    handleMediaQuery(media);
+
+    return () => media.removeListener(handleMediaQuery);
+  }, [handleMediaQuery]);
+
+  // localStorage event handling
+  useEffect(() => {
+    const handleStorage = (e: StorageEvent) => {
+      if (e.key !== storageKey) {
+        return;
+      }
+
+      // If default theme set, use it if localstorage === null (happens on local storage manual deletion)
+      const theme = e.newValue ?? defaultTheme;
+      setTheme(theme);
+    };
+
+    window.addEventListener("storage", handleStorage);
+    return () => window.removeEventListener("storage", handleStorage);
+  }, [defaultTheme, setTheme, storageKey]);
+
+  // Whenever theme or forcedTheme changes, apply it
+  useEffect(() => {
+    applyTheme(forcedTheme ?? theme!);
+  }, [applyTheme, forcedTheme, theme]);
+
+  const providerValue = useMemo(
+    () => ({
+      theme,
+      setTheme,
+      forcedTheme,
+      resolvedTheme: theme === "system" ? resolvedTheme : theme,
+      themes: enableSystem ? [...themes, "system"] : themes,
+      systemTheme: (enableSystem ? resolvedTheme : undefined) as
+        | "light"
+        | "dark"
+        | undefined,
+    }),
+    [theme, setTheme, forcedTheme, resolvedTheme, enableSystem, themes],
+  );
+
+  return (
+    <ThemeContext.Provider value={providerValue as UseThemeProps}>
+      <ThemeScript
+        {...{
+          forcedTheme,
+          disableTransitionOnChange,
+          enableSystem,
+          enableColorScheme,
+          storageKey,
+          themes,
+          defaultTheme,
+          attribute,
+          value,
+          children,
+          attrs,
+          nonce,
+        }}
+      />
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+// eslint-disable-next-line react/display-name, @typescript-eslint/no-explicit-any
+const ThemeScript: any = memo(
+  ({
+    forcedTheme,
+    storageKey,
+    attribute,
+    enableSystem,
+    enableColorScheme,
+    defaultTheme,
+    value,
+    attrs,
+    nonce,
+  }: ThemeProviderProps & { attrs: string[]; defaultTheme: string }) => {
+    const defaultSystem = defaultTheme === "system";
+
+    // Code-golfing the amount of characters in the script
+    const optimization = (() => {
+      if (attribute === "class") {
+        const removeClasses = `c.remove(${attrs
+          .map((t: string) => `'${t}'`)
+          .join(",")})`;
+
+        //   setAttribute("arco-theme", resolvedTheme as string)
+        console.log('removeClasses', attrs)
+        return `var d=document.documentElement,c=d.classList,a=document.body;${removeClasses};document.body.setAttribute('arco-theme', 'dark');`;
+      } else {
+        return `var d=document.documentElement,a=document.body,n='${attribute}',s='setAttribute';`;
+      }
+    })();
+
+    const fallbackColorScheme = (() => {
+      if (!enableColorScheme) {
+        return "";
+      }
+
+      const fallback = colorSchemes.includes(defaultTheme)
+        ? defaultTheme
+        : null;
+
+      if (fallback) {
+        return `if(e==='light'||e==='dark'||!e)d.style.colorScheme=e||'${defaultTheme}'`;
+      } else {
+        return `if(e==='light'||e==='dark')d.style.colorScheme=e`;
+      }
+    })();
+
+    const updateDOM = (
+      name: string,
+      literal = false,
+      setColorScheme = true,
+    ) => {
+      const resolvedName = value ? value[name] : name;
+      const val = literal ? name + `|| ''` : `'${resolvedName}'`;
+      let text = "";
+
+      // MUCH faster to set colorScheme alongside HTML attribute/class
+      // as it only incurs 1 style recalculation rather than 2
+      // This can save over 250ms of work for pages with big DOM
+      if (
+        enableColorScheme &&
+        setColorScheme &&
+        !literal &&
+        colorSchemes.includes(name)
+      ) {
+        text += `d.style.colorScheme = '${name}';`;
+      }
+
+      if (attribute === "class") {
+        if (literal || resolvedName) {
+          text += `c.add(${val});a.setAttribute('arco-theme', ${val})`;
+        } else {
+          text += `null`;
+        }
+      } else {
+        if (resolvedName) {
+          text += `d[s](n,${val})`;
+        }
+      }
+
+      console.log('text', text)
+      return text
+    };
+
+    const scriptSrc = (() => {
+      if (forcedTheme) {
+        return `!function(){${optimization}${updateDOM(forcedTheme)}}()`;
+      }
+
+      if (enableSystem) {
+        return `!function(){try{${optimization}var e=localStorage.getItem('${storageKey}');if('system'===e||(!e&&${defaultSystem})){var t='${MEDIA}',m=window.matchMedia(t);if(m.media!==t||m.matches){${updateDOM(
+          "dark",
+        )}}else{${updateDOM("light")}}}else if(e){${
+          value ? `var x=${JSON.stringify(value)};` : ""
+        }${updateDOM(value ? `x[e]` : "e", true)}}${
+          !defaultSystem
+            ? `else{` + updateDOM(defaultTheme, false, false) + "}"
+            : ""
+        }${fallbackColorScheme}}catch(e){}}()`;
+      }
+
+      return `!function(){try{${optimization}var e=localStorage.getItem('${storageKey}');if(e){${
+        value ? `var x=${JSON.stringify(value)};` : ""
+      }${updateDOM(value ? `x[e]` : "e", true)}}else{${updateDOM(
+        defaultTheme,
+        false,
+        false,
+      )};}${fallbackColorScheme}}catch(t){}}();`;
+    })();
+
+    return (
+      <script nonce={nonce} dangerouslySetInnerHTML={{ __html: scriptSrc }} />
+    );
+  },
+  // Never re-render this component
+  () => true,
+);
+
+// Helpers
+const getTheme = (key: string, fallback?: string) => {
+  if (isServer) return undefined;
+  let theme;
+  try {
+    theme = localStorage.getItem(key) ?? undefined;
+  } catch (e) {
+    // Unsupported
+  }
+  return theme ?? fallback;
+};
+
+const disableAnimation = () => {
+  const css = document.createElement("style");
+  css.appendChild(
+    document.createTextNode(
+      `*{-webkit-transition:none!important;-moz-transition:none!important;-o-transition:none!important;-ms-transition:none!important;transition:none!important}`,
+    ),
+  );
+  document.head.appendChild(css);
+
+  return () => {
+    // Force restyle
+    (() => window.getComputedStyle(document.body))();
+
+    // Wait for next tick before removing
+    setTimeout(() => {
+      document.head.removeChild(css);
+    }, 1);
+  };
+};
+
+const getSystemTheme = (e?: MediaQueryList | MediaQueryListEvent) => {
+  if (!e) e = window.matchMedia(MEDIA);
+  const isDark = e.matches;
+  const systemTheme = isDark ? "dark" : "light";
+  return systemTheme;
+};
+// import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+// import { ThemeProvider, useTheme } from "next-themes";
+
+// import { Button } from "./button";
+// import {
+//   DropdownMenu,
+//   DropdownMenuContent,
+//   DropdownMenuItem,
+//   DropdownMenuTrigger,
+// } from "./dropdown-menu";
+
+// function ThemeToggle() {
+//   const { setTheme, resolvedTheme } = useTheme();
+
+//   useEffect(() => {
+//     document.body.setAttribute("arco-theme", resolvedTheme as string);
+//   }, [resolvedTheme]);
+
+//   return (
+//     <DropdownMenu>
+//       <DropdownMenuTrigger asChild>
+//         <Button variant="outline" size="icon">
+//           <SunIcon className="size-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+//           <MoonIcon className="absolute size-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+//           <span className="sr-only">Toggle theme</span>
+//         </Button>
+//       </DropdownMenuTrigger>
+//       <DropdownMenuContent align="end">
+//         <DropdownMenuItem onClick={() => setTheme("light")}>
+//           Light
+//         </DropdownMenuItem>
+//         <DropdownMenuItem onClick={() => setTheme("dark")}>
+//           Dark
+//         </DropdownMenuItem>
+//         <DropdownMenuItem onClick={() => setTheme("system")}>
+//           System
+//         </DropdownMenuItem>
+//       </DropdownMenuContent>
+//     </DropdownMenu>
+//   );
+// }
+
+// export { ThemeProvider, ThemeToggle };
+
+export function ArcoThemeProvider({ children }: { children: React.ReactNode }) {
+  const { setTheme } = useTheme();
+
+  return (
+    <>
+      <ConfigProvider>{children}</ConfigProvider>
+      <div className="absolute bottom-4 right-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <SunIcon className="size-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <MoonIcon className="absolute size-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </>
+  );
+}
