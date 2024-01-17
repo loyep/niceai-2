df9a72067a6d248918bc9f3ef7e812347c7a62fc
Zack <lorenx@163.com>
2024-01-18 01:05:05 +0800

    feat: update db schema

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -4,16 +4,16 @@ import { createPool as createMysql2Poll } from "mysql2/promise";
 import { connect as createConnect } from "@planetscale/database";
 import { drizzle as planetscaleDrizzle } from "drizzle-orm/planetscale-serverless";
 
-export function createDB({ url, mode, schema }: { url: string; mode: "planetscale" | "mysql2"; schema: Record<string, unknown> }) {
+export function createDB<TSchema extends Record<string, unknown> = Record<string, never>>({ url, mode, schema }: { url: string; mode: "planetscale" | "mysql2"; schema: TSchema }) {
   if (mode === "planetscale") {
     const connection = createConnect({
       url,
     });
 
-    return planetscaleDrizzle(connection, { schema });
+    return planetscaleDrizzle<TSchema>(connection, { schema });
   }
 
   const connection = createMysql2Poll(url);
 
-  return mysql2Drizzle(connection, { schema, mode: 'default' });
+  return mysql2Drizzle<TSchema>(connection, { schema, mode: 'default' });
 }
