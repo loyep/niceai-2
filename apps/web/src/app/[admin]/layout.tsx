0433dfe6d82c86217fc28efe5cb751bb98796d31
Zack <lorenx@163.com>
2024-01-19 03:37:46 +0800

    update layout

--- apps/web/src/app/[admin]/layout.tsx
+++ apps/web/src/app/[admin]/layout.tsx
@@ -1,11 +1,16 @@
-import { notFound } from "next/navigation";
 import { PropsWithChildren } from "react";
+import { notFound } from "next/navigation";
+
+import Layout from "../../layouts/admin";
 
 type Props = {
-  params: { admin: string }
-}
+  params: { admin: string };
+};
 
-export default function AdminLayout({ children, params }: PropsWithChildren<Props>) {
+export default function AdminLayout({
+  children,
+  params,
+}: PropsWithChildren<Props>) {
   if (params.admin !== "cloud") notFound();
-  return children;
+  return <Layout>{children}</Layout>;
 }
--- apps/web/src/app/[admin]/page.tsx
+++ apps/web/src/app/[admin]/page.tsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 
 import { useSession } from "@niceai/auth/react";
 
 export default function AdminPage() {
   const session = useSession();
-  if (session.data?.user?.email !== 'lorenx@163.com') {
+  if (session.data?.user?.email !== "lorenx@163.com") {
     // return notFound();
     return <div>Not Found</div>;
   }
