1c7256ea70144462e76399271a38d65847b022e1
Zack <lorenx@163.com>
2024-01-29 21:18:13 +0800

    update

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -14,21 +14,21 @@ const config = {
     "@niceai/trpc",
     "@niceai/validators",
   ],
-
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/docs",
+  //       destination: "https://docs.niceai.io/docs",
+  //     },
+  //     {
+  //       source: "/docs/:path*",
+  //       destination: "https://docs.niceai.io/docs/:path*",
+  //     },
+  //   ];
+  // },
 };
 
 export default config;
